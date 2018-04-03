4deb5031f76c4f431aa3bc2c59359283a9af74ba
Christopher Auer <christopher.auer@web.de>
2018-04-03 20:33:52 +0200

    fixed copyright info

--- examples/AdhocMacros/AdhocMacros.ino
+++ examples/AdhocMacros/AdhocMacros.ino
@@ -1,6 +1,6 @@
 /* -*- mode: c++ -*-
- * Kaleidoscope-KeyLogger -- A key logger
- * Copyright (C) 2016, 2017  Gergely Nagy
+ * Kaleidoscope-AdhocMacros 
+ * Copyright (C) 2018  Christopher Auer
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,33 +17,7 @@
  */
 
 #include <Kaleidoscope.h>
-#include <Kaleidoscope-KeyLogger.h>
+#include <Kaleidoscope-AdhocMacros.h>
 
-const Key keymaps[][ROWS][COLS] PROGMEM = {
-  [0] = KEYMAP_STACKED
-  (Key_NoKey, Key_1, Key_2, Key_3, Key_4, Key_5, Key_NoKey,
-   Key_Backtick,      Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,        Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown,      Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
+// TODO
 
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-   Key_NoKey,
-
-   Key_skip,  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_skip,
-   Key_Enter, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
-   Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
-   Key_skip,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
-
-   Key_RightShift, Key_RightAlt, Key_Spacebar, Key_RightControl,
-   Key_NoKey),
-};
-
-void setup() {
-  Kaleidoscope.use(&KeyLogger);
-
-  Kaleidoscope.setup();
-}
-
-void loop() {
-  Kaleidoscope.loop();
-}
